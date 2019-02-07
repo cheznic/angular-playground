29412fa4e5486edbdc34b35e44c69b51162d6393
Charles Nicoletti <cnicoletti@gmail.com>
2019-02-06 19:04:52 -0800

    adding recipe-book

---
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class DataStorageService {
+   constructor(
+      private http: Http,
+      private recipeService: RecipeService,
+      private authService: AuthService
+   ) { }
+
+   storeRecipes() {
+      const token = this.authService.getToken();
+      this.http.put(
+         'https://ng-recipe-book-1ba03.firebaseio.com/recipes.json?auth=' + token,
+         this.recipeService.getRecipes()
+      ).subscribe(
+         (response: Response) => {
+            // TODO: if error...
+         }
+      );
+
+   }
+
+   getRecipes() {
+      const token = this.authService.getToken();
+      this.http.get('https://ng-recipe-book-1ba03.firebaseio.com/recipes.json?auth=' + token)
+         .pipe(map(
+            (response: Response) => {
+               const recipes: Recipe[] = response.json();
+               for (let recipe of recipes) {
+                  if (!recipe['ingredients']) {
+                     recipe['ingredients'] = [];
+                  }
+               }
+               return recipes;
+            }
+         ))
+         .subscribe(
+            (recipes: Recipe[]) => {
+               this.recipeService.saveRecipes(recipes);
+            }
+         );
+   }
+}
